5022fe8570bfaf61a6008281eab89a1a62fd5750
”Anna <hobbita@yandex.ru>
2023-08-31 23:27:07 +0300

    4.2

---
+++ HW4_2.java
@@ -0,0 +1,12 @@
+public class HW4_2 {
+
+
+        public static void main(String[] args) {
+            /*Необходимо вывести числа от 0 до 15.*/
+
+            for (int i=0;i<16;i=i+1){
+                System.out.println(i);
+            }
+        }
+
+}
